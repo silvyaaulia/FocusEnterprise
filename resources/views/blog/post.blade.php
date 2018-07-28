468476c7ec72edf3f4d7ef5b538c5c198be9d927
Silvya Aulia <silvyaaulia@gmail.com>
2018-07-28 20:19:42 +0700

    edit post

--- resources/views/blog/post.blade.php
+++ resources/views/blog/post.blade.php
@@ -1,46 +1,39 @@
 @extends('layouts\master')
 
-
-@section('csscontent')
-.post{
-    font-family: 'Berlin Sans FB', sans-serif;
-}
-#judulpost{
-  font-size:24pt;
-}
-#dateandtime{
-  font-size:10pt;
-}
-.pcentre, img.pcentre{
-  text-align: center;
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-  width: 60%;
-}
-.pjustify{
-  text-align:justify;
-}
-
-
-@endsection
-
-
 @section('content')
 <div class="post">
+  <a id="allevents" href="#">All events</a>
+  <p id='posttitle' style= "text-align:left">Judul Posting Berita Focus</p>
+  <p id="dateandtime">21 Juli 2018, 8am-9am</p>
+  <img class="img-pcentre" src="/images/pic_dekorasi pernikahan.jpg" alt="">
+  <p class="pjustify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
-<p id='judulpost'>Judul Posting Berita Focus</p>
-<p id='dateandtime'>21 Juli 2018, 8.00 am</p>
+  <div class="detailpost">
+    <a id="detailpost-btn" href="#">Google Maps</a>
+    <a id="detailpost-btn" href="#">Google Calender</a>
+    <a id="detailpost-btn" href="#">Ical Export</a>
+    <div class="detailpost-info">
+      <p class="pcenter"><b>Details</b></p>
+      <p class="pjustify">
+        Date:<br>21 Juli 2018 <br>
+        time:<br> 8am-9am <br>
+        Event tag: <br> dekorasi pernikahan, paket pernikahan, lengkap <br>
+      </p>
 
-<img class="pcentre" src="storage\images\logo-facebook.png">
+      <p class="pcenter"><b>Organizer</b></p>
+      <p class="pjustify">
+        Focus Entreprise <br>
+        phone: <br> 082x-xxxx-xxx <br>
+        Email: <br> ini@email.com <br>
+        Website: <br> focusentp <br>
+      </p>
 
-<p class="pjustify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-
+      <p class="pcenter"><b>Venue</b></p>
+      <p class="pjustify">
+        SMKN57 <br>
+        alamatnya Jalan Raya no 7 kota Baru <br>
+      </p>
+    </div>
+  </div>
 </div>
-
-style= "text-align:center"
-height:500px;width:800px;
-overflow:hidden;background:none!important;
-
 @endsection
