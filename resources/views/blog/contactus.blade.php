0d6ad6f488afb14d93726b011124bf5d37bd72ee
Silvya Aulia <silvyaaulia@gmail.com>
2018-07-28 16:31:29 +0700

    edit contactus

--- resources/views/blog/contactus.blade.php
+++ resources/views/blog/contactus.blade.php
@@ -1,45 +1,18 @@
 @extends('layouts\master')
 
-@section('csssubheader')
-#judulhalaman{
-  text-align:center;
-  font-family: 'Bebas Neue', sans-serif;
-  font-size: 30pt;
-}
-p{
-  text-align:center;
-  font-family: 'Berlin Sans FB', sans-serif;
-  backround-image: url('src="storage\images\logo-facebook.png"')
-}
-@endsection
-
-
-@section('csscontent')
-.contactus{
-    font-family: 'Berlin Sans FB', sans-serif;
-}
-#judul{
-  font-size:35pt;
-  padding:50px;
-}
-ul{
-  list-style-type:none;
-  text-indent: 100px;
-}
-@endsection
-
-
-
-
-@section('subheader')
-<p id='judulhalaman'>Contact Us</p>
-<p>home>>contact us</p>
+@section('pagetitle')
+<div class="pagetitle-bg">
+  <p id="pagetitle">Contact Us</p>
+  <p id="pagetitle-addr">home>>contact us</p>
+</div>
 @endsection
 
 @section('content')
 <div class="contactus">
-<p id='judul' style= "text-align:center">Focus Enteprice</p>
-<ul>
+  <p id='contenttitle' style= "text-align:center">Focus Entreprise</p>
+  <ul>
+  <li style="font-size:13pt">CV Focus Entreprise</li>
+  <br>
   <li>Alamat:</li>
   <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed</li>
   <br>
@@ -48,28 +21,15 @@ ul{
   <br>
   <li>Email:</li>
   <li>ini@email.com</li>
-</ul>
+  </ul>
 </div>
-
 
 <div class="mapouter">
   <div class="gmap_canvas">
-    <iframe width="900" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=itb%20bandung&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
-    <ahref="https://www.embedgooglemap.net">embedgooglemap.net</a>
+    <iframe width="1200" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=itb%20bandung&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+    <ahref="https://www.embedgooglemap.net"></a>
   </div>
-  <style>
-  .mapouter{
-  text-align:center;height:500px;width:800px;
-  }
-  .gmap_canvas {
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    width:100%;
-  }
-  </style>
 </div>
-height:500px;width:800px;
-overflow:hidden;background:none!important;
+
 
 @endsection
