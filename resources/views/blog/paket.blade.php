ac7fbe50960e2eec318c2e5e560e6ceb400d378a
Silvya Aulia <silvyaaulia@gmail.com>
2018-08-02 09:54:49 +0700

    edit font & edit ourservice#2

--- resources/views/blog/paket.blade.php
+++ resources/views/blog/paket.blade.php
@@ -13,7 +13,7 @@
   <img class="img-pcentre" src="/images/pic_dekorasi pernikahan.jpg" alt="">
   <p class="pcentre">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   <p class="pcentre">Download harga paket A</p>
-  <img class="img-pcentre" src="/images/logo_download.png" href="#" height="30px" width="30px">
+  <img class="img-pcentre" src="/images/logo-download.png" href="#" height="30px" width="30px">
 
 
 <table>
@@ -36,7 +36,7 @@
       <br>
       Nasi uduk
       <br>
-      Aneka Soup 
+      Aneka Soup
     </td>
   </tr>
 </table>
